58e9d2b20705d528b9f0644931a63b23c745ee9b
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2025-11-17 17:48:15 +0700

    add Product controller

---
+++ E-Commerce _API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using E_Commerce__API.Data;
+using E_Commerce__API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce__API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = context.Products.ToList();
+
+            return Ok(products);
+        }
+        [HttpGet("{id}")] //api/products/1
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
